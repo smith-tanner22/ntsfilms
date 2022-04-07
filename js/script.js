013223913cf20278a4c7e92a7d9b537563b55021
N. Tanner Smith <smith.tanner2222@gmail.com>
2022-04-07 10:39:27 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const copyrightyear = document.getElementById("currentdate");
+copyrightyear.textContent = new Date().getFullYear();
+
+var lastmodified = document.getElementById("lastmodified");
+lastmodified.textContent = new Date(document.lastModified);
