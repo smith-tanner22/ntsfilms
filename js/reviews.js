eae1ec2bd6e067c689166dbc5c4dc498aac3cf3b
smith-tanner22 <smith.tanner2222@gmail.com>
2022-07-11 23:40:37 -0600

    added some homepage stuff and fixed some settings

--- js/reviews.js
+++ js/reviews.js
@@ -10,13 +10,21 @@ fetch(requestURL)
     const reviews = jsonObject['reviews'];
 
     for (let i = 0; i < reviews.length; i++) {
+      let testimonial = document.createElement('div');
       let card = document.createElement('section');
+      card.classList.add('cardSection');
       let h2 = document.createElement('h2');
+      h2.classList.add('cardTitle');
       let type = document.createElement('p');
+      type.classList.add('cardType');
       let date = document.createElement('p');
+      date.classList.add('cardDate');
       let rating = document.createElement('p');
+      rating.classList.add('cardRating');
       let review = document.createElement('p');
+      review.classList.add('cardReview');
       let img = document.createElement('img');
+      img.classList.add('cardImg');
 
       h2.textContent = reviews[i].name;
       type.textContent = 'Type:' + reviews[i].type;
@@ -29,6 +37,13 @@ fetch(requestURL)
         reviews[i].name + ' ' + reviews[i].type + 'with ' + reviews[i].name
       );
 
+      // testimonial.appendChild(card);
+      // testimonial.appendChild(h2);
+      // testimonial.appendChild(type);
+      // testimonial.appendChild(date);
+      // testimonial.appendChild(rating);
+      // testimonial.appendChild(review);
+      // testimonial.appendChild(img);
       card.appendChild(h2);
       card.appendChild(type);
       card.appendChild(date);
@@ -36,6 +51,6 @@ fetch(requestURL)
       card.appendChild(review);
       card.appendChild(img);
 
-      document.querySelector('div.cards').appendChild(card);
+      document.querySelector('div.testimonial-box-container').appendChild(card);
     }
   });
--- js/script.js
+++ js/script.js
@@ -3,22 +3,3 @@ copyrightyear.textContent = new Date().getFullYear();
 
 var lastmodified = document.getElementById('lastmodified');
 lastmodified.textContent = new Date(document.lastModified);
-
-const menuButton = document.querySelector('.ham');
-const mainNav = document.querySelector('.navigation');
-
-menuButton.addEventListener(
-  'click',
-  () => {
-    console.log('button was clicked');
-    mainNav.classList.toggle('responsive');
-  },
-  false
-);
-
-window.onresize = () => {
-  if (window.innerWidth > 768) {
-    console.log('window was resized');
-    mainNav.classList.remove('responsive');
-  }
-};
